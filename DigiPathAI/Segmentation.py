43cc5749201af89d78c1122d3715c148a4115398
Jack Breen <77981425+scjjb@users.noreply.github.com>
2021-12-06 15:46:31 +0000

    Allow tf version 2
    Adjust tensorflow random seed to work on either version 1 or version 2

--- DigiPathAI/Segmentation.py
+++ DigiPathAI/Segmentation.py
@@ -56,8 +56,10 @@ home = expanduser("~")
 # Random Seeds
 np.random.seed(0)
 random.seed(0)
-tf.set_random_seed(0)
-
+if(int(tf.__version__[0])>1):
+    tf.random.set_seed(0)
+else:
+    tf.set_random_seed(0)
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
